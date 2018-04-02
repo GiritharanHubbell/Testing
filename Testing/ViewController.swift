b3b8ba84f206696cbb2c68e77656b0fb5723557d
EEE Common <eeecommon@hubbell.com>
2018-04-02 17:19:29 +0530

    A For apple Added

--- Testing/ViewController.swift
+++ Testing/ViewController.swift
@@ -12,6 +12,7 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        print("A for Apple")
         // Do any additional setup after loading the view, typically from a nib.
     }
 
